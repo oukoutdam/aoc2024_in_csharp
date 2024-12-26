ac855680125c3b4872c1a3df38dc814348867a26
oukoutdam <outdam.ouk103@gmail.com>
2024-12-27 00:23:40 +0900

    Finish Day Part 1

--- Day1_1/Program.cs
+++ Day1_1/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string filePath = "./Day1_1/input.txt";
+List<int> left = [];
+List<int> right = [];
+
+try
+{
+    string[] lines = File.ReadAllLines(filePath);
+    foreach (string line in lines)
+    {
+        string[] values = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        if (values.Length == 2)
+        {
+            left.Add(int.Parse(values[0]));
+            right.Add(int.Parse(values[1]));
+        }
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error: {ex.Message}");
+}
+
+left.Sort();
+right.Sort();
+
+int sum = 0;
+for (int i = 0; i < left.Count; i++)
+{
+    var val = left[i] - right[i];
+    sum += (val > 0) ? val : -val;
+}
+
+Console.WriteLine(sum);
