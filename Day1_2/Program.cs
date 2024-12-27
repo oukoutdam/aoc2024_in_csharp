fb7b1c57bb6da64117e4ca7520b20539f166e380
Ouk Outdam <outdam.ouk103@gmail.com>
2024-12-28 01:03:57 +0900

    Read File for Day2

--- Day1_2/Program.cs
+++ Day1_2/Program.cs
@@ -1,6 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program {
+	public static void Main(){
+		string filePath = "./input.txt";
 
-string fileName = "data.json";
-string dataFilePath = Path.Combine(Directory.GetCurrentDirectory(), fileName);
-Console.WriteLine(dataFilePath);
+		List<int> left = [];
+		List<int> right = [];
+
+		try {
+			string[] lines = File.ReadAllLines(filePath);
+
+			foreach(var line in lines){
+				string[] values = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+				if (values.Length == 2){
+					left.Add(int.Parse(values[0]));
+					right.Add(int.Parse(values[1]));
+				}
+			}
+
+		} catch (Exception ex){
+			Console.WriteLine($"Error: {ex.Message}");
+		}
+
+		Console.WriteLine(left.Count);
+
+		
+
+	}
+}
